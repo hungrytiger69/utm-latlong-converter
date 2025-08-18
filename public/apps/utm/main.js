2fd91afa0e0905087354a19d92f62b956f781383
hungry_tiger_49 <mattjdunkerley@gmail.com>
2025-08-17 22:19:30 -0400

    cleaned up the landing page

--- public/apps/utm/main.js
+++
@@ -1,5 +0,0 @@
-// Paste your working converter here. For now, a sanity check:
-(() => {
-  console.info('UTM app booting… proj4 loaded?', !!window.proj4);
-  // Your converter code goes below this line.
-})();
